fb45b856e700efae53903ba199526cb99115d6cc
Jack Parsons <jparsons@ualberta.ca>
2025-12-24 12:00:42 -0700

    simple app runner

---
+++ build.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const runtime_dep = b.dependency("zephyr_runtime", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const runtime_mod = runtime_dep.module("zephyr_runtime");
+
+    const exe = b.addExecutable(.{
+        .name = "zephyr_sandbox",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "zephyr_runtime", .module = runtime_mod },
+            },
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+}
---
+++ src/main.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const runtime = @import("zephyr_runtime");
+
+pub const std_options = runtime.recommended_std_options;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const application = try runtime.Application.init(allocator);
+    defer application.deinit(allocator);
+    application.run();
+}
